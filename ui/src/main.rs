135debe334550efc96b9050f88c662ba10328bd5
Josh Espinoza <joshespi@gmail.com>
2023-06-10 10:12:01 -0600

    modified header

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -23,20 +23,17 @@ impl Component for App {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <div class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" id="mainHeader">
                 <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <img src="/data/images/banner-background.jpg" alt="" />
+                    <img src="/data/images/banner-background.jpg" alt="Espi Avatar" />
                     <span class="fs-4">{"Simple header"}</span>
                 </a>
                 <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">{"Home"}</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">{"Features"}</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">{"Pricing"}</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">{"FAQs"}</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link" aria-current="page">{"Home"}</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">{"About"}</a></li>
                 </ul>
                 
-            </div>
+            </header>
 
           }
     }
