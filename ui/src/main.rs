5a702103a80b7a5ac8535021b58b6390be6f004e
Josh Espinoza <joshespi@gmail.com>
2023-06-10 18:19:40 -0600

    finish part 2

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,11 +1,20 @@
+mod pages;
+
+use pages::home::Home;
+use pages::not_found::NotFound;
 use yew::html;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 //Create the main app that will load all other Components
-pub struct App {}
+pub struct App {
+    navbar_active: bool,
+}
 
 //Message enum that is used for managing the life cycle of Components
-pub enum Msg {}
+pub enum Msg {
+    ToggleNavbar,
+}
 
 //Implement the Component interface
 impl Component for App {
@@ -14,28 +23,101 @@ impl Component for App {
 
     //Create a new App
     fn create(_ctx: &Context<Self>) -> Self {
-        App {}
+        App {
+            navbar_active: false,
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        false
+        match msg {
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
+                true
+            }
+        }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom" id="mainHeader">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <img src="/data/images/banner-background.jpg" alt="Espi Avatar" />
-                    <span class="fs-4">{"Simple header"}</span>
-                </a>
-                <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="#" class="nav-link" aria-current="page">{"Home"}</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">{"About"}</a></li>
-                </ul>
-                
-            </header>
+          <BrowserRouter>
+              {self.view_nav(&ctx)}
+            <main>
+                <Switch<Route> render={Switch::render(switch)} />
+            </main>
+          </BrowserRouter>
 
-          }
+        }
+    }
+}
+
+impl App {
+    fn view_nav(&self, ctx: &Context<Self>) -> Html {
+        let Self { navbar_active } = *self;
+
+        let active_class = if !navbar_active {
+            "collapse navbar-collapse"
+        } else {
+            "navbar-collapse collapse show"
+        };
+        html! {
+            <nav class="navbar navbar-expand-lg p-2 sticky-top navbar-dark bg-dark">
+
+                <Link<Route> classes={classes!("navbar-brand")} to={Route::Home}>
+                    {"Rust Website"}
+                </Link<Route>>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                    onclick={ctx.link().callback(|_| Msg::ToggleNavbar)}
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class={classes!(active_class)} id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                                <Link<Route> classes={classes!("nav-link")} to={Route::Home}>
+                                    { "Home" }
+                                </Link<Route>>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="https://github.com/nmharmon8/Rust_Rocket_Yew_Tutorial" class="nav-link">
+                            {"GitHubCode"}
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="https://theadventuresofaliceandbob.com/" class="nav-link">
+                            {"Blog"}
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </nav>
+        }
+    }
+}
+
+
+#[derive(Routable, PartialEq, Clone, Debug)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => {
+            html! { <Home /> }
+        }
+        _ => {
+            html! { <NotFound /> }
+        }
     }
 }
 
---
+++ ui/src/pages/home.rs
@@ -0,0 +1,20 @@
+use yew::prelude::*;
+
+pub struct Home;
+
+impl Component for Home {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Home {}
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div class="d-flex justify-content-center m-5">
+                <h1>{"Building a Website in Rust"}</h1>
+            </div>
+        }
+    }
+}
